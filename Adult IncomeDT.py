33c0f656cd3540c71c42bdd31e71adaed53b107c
Dharana <47414442+Dharana23@users.noreply.github.com>
2020-03-29 12:36:14 +0530

    Decision Tree

---
+++ Adult IncomeDT.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+data = pd.read_csv('C:/Users/kain_/Downloads/Course Files/007 - Classification/Adult Income.csv')
+data.isnull().sum(axis=0)
+data.dtypes
+
+data_prep = pd.get_dummies(data, drop_first=True)
+
+X = data_prep.iloc[:, :-1]
+Y = data_prep.iloc[:, -1]
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=1234, stratify=Y)
+
+from sklearn.metrics import confusion_matrix
+from sklearn.tree import DecisionTreeClassifier
+
+dtc = DecisionTreeClassifier(random_state=1234)
+dtc.fit(X_train, y_train)
+y_predict = dtc.predict(X_test)
+
+cm = confusion_matrix(y_test, y_predict)
+score = dtc.score(X_test, y_test)
